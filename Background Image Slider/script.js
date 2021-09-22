ff66009e9cc90bfc5d04531b07855de4ae1f1681
tudor0 <tudor.tuglea@gmail.com>
2021-09-22 21:24:25 +0300

    gi

--- Background Image Slider/script.js
+++ Background Image Slider/script.js
@@ -1,7 +1,7 @@
 let left = document.getElementById("left");
 let right = document.getElementById("right");
 let pictureArray = [
-  "cat.jpg",
+  "images/cat.jpg",
   "images/p1.jpg",
   "images/p2.jpg",
   "images/p3.jpg",
@@ -10,32 +10,21 @@ let pictureArray = [
   "images/p6.jpg",
   "images/counter.jpg",
 ];
-let l = pictureArray.length;
+
 let image = document.getElementById("imageid");
 let index = 0;
 left.addEventListener("click", () => {
   index--;
-  image.src = pictureArray[index];
-  if (index <= 0) {
-    index = l;
+  if (index <= -1) {
+    index = 7;
   }
+  image.src = pictureArray[index];
 });
 
 right.addEventListener("click", () => {
   index++;
-  console.log(index);
-  if (index === l) {
+  if (index >= 7) {
     index = 0;
   }
   image.src = pictureArray[index];
 });
-
-// function update(index, l) {
-//   console.log(`The index is ${index} and l is ${l}`);
-//   if (index >= l) {
-//     index = 0;
-//   }
-//   if (index <= l) {
-//     index = l;
-//   }
-// }
